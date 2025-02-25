8558acca6eae9d7cdf18906ff3e337b809590620
abhitrambadiya <abhitrambadiya@hotmail.com>
2025-02-25 19:22:55 +0530

    index js update origins

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(
     cors({
       origin: (origin, callback) => {
         const allowedOrigins = [
-          "http://localhost:5173",
+          // "http://localhost:5173",
           "https://abhitrambadiya.onrender.com"
         ];
         if (!origin || allowedOrigins.includes(origin)) {
