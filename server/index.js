71c34c0c9ef4305ca48908be079624b1d91920e8
abhitrambadiya <abhitrambadiya@hotmail.com>
2025-02-25 20:56:07 +0530

    Update index js

--- server/index.js
+++ server/index.js
@@ -32,8 +32,19 @@ app.use(cors({ origin: "https://abhitrambadiya.onrender.com" })); // Allow front
 // Allow all origins
 app.use(express.json());
 
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+// ✅ Serve React frontend
+app.use(express.static(path.join(__dirname, "../client/dist")));
+
 // Use email route
 app.use("/api/email", emailRoute);
+
+// ✅ Catch-all route for React Router
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
+});
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
